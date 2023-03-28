6d315b0131c8f15add82938d1197058dbf2a4016
proksch <proksch@E4CDEL001>
2023-03-28 14:35:38 +0200

    Projektdateien hinzufügen.

---
+++ EmpowerTP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3A53D31D-E3B2-4984-AC78-E4B1B55B2A31")]
+[assembly: AssemblyVersion("1.0.0.0")]
